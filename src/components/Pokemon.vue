a2c43c8a001b62de97af637bcdfc840e4ffcebd5
brenosekiro <b88660501@gmail.com>
2020-11-13 17:15:51 -0300

    getting pokemons types and sprites

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,12 +6,25 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Pokemon',
     props: {
         name: String,
         url: String,
         num: Number
+    },
+    created: async function () {
+        const pokemon = await axios.get(this.url)
+        this.pokemon.type = pokemon.data.types[0].type.name
+        this.pokemon.front = pokemon.data.sprites.front_default
+        this.pokemon.back = pokemon.data.sprites.back_default
+    },
+    data() {
+        return {
+            pokemon: {}
+        }
     }
 }
 </script>
